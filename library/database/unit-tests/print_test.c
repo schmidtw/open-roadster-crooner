b6728b9d86c7016fd42dc18ca6366e46913c66b2
Trent Schmidt <trent.schmidt@gmail.com>
2009-06-04 06:13:24 +0000

    More test cases...still not passing all the tests though

--- library/database/unit-tests/print_test.c
+++ library/database/unit-tests/print_test.c
@@ -12,13 +12,18 @@ char artist[MAX_ARTIST_NAME_W_NULL];
 char album[MAX_ALBUM_TITLE_W_NULL];
 char song[MAX_SONG_TITLE_W_NULL];
 
+void fake_audio_control( void )
+{
+    printf("fake_audio_control called\n");
+}
+
 void create_simple_database( void )
 {
     group_node_t * group;
     song_node_t * so_n;
     ll_node_t * node;
     
-    node = get_new_group_and_node("1\0");
+    node = get_new_group_and_node("1");
     CU_ASSERT( NULL != node );
     ll_append( &rdn.groups, node );
     rdn.size_list++;
@@ -29,24 +34,24 @@ void create_simple_database( void )
     sprintf(artist, "A Me");
     sprintf(album, "Brushfire Fairytales");
     sprintf(song, "Inaudible Melodies");
-    so_n = add_song_to_group(group, artist, album, song, 1, NULL, NULL, NULL);
+    so_n = add_song_to_group(group, artist, album, song, 1, fake_audio_control, fake_audio_control, "Here");
     CU_ASSERT( NULL != so_n );
     
     sprintf(artist, "Jack Johnson");
     sprintf(album, "Brushfire Fairytales");
     sprintf(song, "Inaudible Melodies");
-    so_n = add_song_to_group(group, artist, album, song, 2, NULL, NULL, NULL);
+    so_n = add_song_to_group(group, artist, album, song, 2, fake_audio_control, fake_audio_control, "Here");
     CU_ASSERT( NULL != so_n );
     sprintf(song, "Middle Man");
-    so_n = add_song_to_group(group, artist, album, song, 1, NULL, NULL, NULL);
+    so_n = add_song_to_group(group, artist, album, song, 1, fake_audio_control, fake_audio_control, "Here");
     CU_ASSERT( NULL != so_n );
     
     sprintf(album, "In between dreams");
     sprintf(song, "Better Together");
-    so_n = add_song_to_group(group, artist, album, song, 1, NULL, NULL, NULL);
+    so_n = add_song_to_group(group, artist, album, song, 1, fake_audio_control, fake_audio_control, "Here");
     CU_ASSERT( NULL != so_n );
     sprintf(song, "Never Know");
-    so_n = add_song_to_group(group, artist, album, song, 2, NULL, NULL, NULL);
+    so_n = add_song_to_group(group, artist, album, song, 2, fake_audio_control, fake_audio_control, "Here");
     CU_ASSERT( NULL != so_n );
 }
 
@@ -89,11 +94,29 @@ void test_next_song( void )
     print_song_info( so_n );
 }
 
+void test_previous_song( void )
+{
+    song_node_t * so_n = NULL;
+    create_simple_database();
+    printf("\n");
+    CU_ASSERT( DS_FAILURE == next_song(NULL, DT_PREVIOUS, DL_SONG) );
+    
+    CU_ASSERT( DS_SUCCESS == next_song(&so_n, DT_PREVIOUS, DL_SONG) );
+    print_song_info( so_n );
+    CU_ASSERT( DS_SUCCESS == next_song(&so_n, DT_PREVIOUS, DL_ARTIST) );
+    print_song_info( so_n );
+    CU_ASSERT( DS_END_OF_LIST == next_song(&so_n, DT_PREVIOUS, DL_SONG) );
+    print_song_info( so_n );
+    CU_ASSERT( DS_SUCCESS == next_song(&so_n, DT_PREVIOUS, DL_ALBUM) );
+    print_song_info( so_n );
+}
+
 void add_suites( CU_pSuite *suite )
 {
     *suite = CU_add_suite( "Print Test", NULL, NULL );
     CU_add_test( *suite, "Test Simple Print   ", test_simple_test );
     CU_add_test( *suite, "Test Next Song Print", test_next_song );
+    CU_add_test( *suite, "Test Prev Song Print", test_previous_song );
 }
 
 int main( int argc, char *argv[] )
