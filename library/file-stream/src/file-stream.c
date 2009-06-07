19199fb9ac2e007db66f347bd2df38c667ec997f
Weston Schmidt <weston_schmidt@alumni.purdue.edu>
2009-06-07 10:39:23 +0000

    [#2](status:fixed) Reworked the library to take advantage of the OS queues and a much simplified control scheme to allow for being stopped mid-stream.

--- library/file-stream/src/file-stream.c
+++ library/file-stream/src/file-stream.c
@@ -15,33 +15,26 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
-#undef PERFORMANCE_METRIC_OUTPUT
 
 
 #include <string.h>
 #include <stdio.h>
 #include <freertos/task.h>
 #include <freertos/semphr.h>
-#include <linked-list/linked-list.h>
+#include <freertos/queue.h>
 #include <fatfs/ff.h>
-
-#ifdef PERFORMANCE_METRIC_OUTPUT
-#include <bsp/abdac.h>
-#endif
 
 #include "file-stream.h"
 
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */
 /*----------------------------------------------------------------------------*/
-#define FSTREAM_TASK_STACK_SIZE         1000
+#define FSTREAM_TASK_STACK_SIZE         (configMINIMAL_STACK_SIZE + 400)
 #define FSTREAM_BIG_BUFFER_SIZE         (32*1024)
 #define FSTREAM_TOTAL_BUFFER_SIZE       (128*1024)
 #define FSTREAM_SMALL_BUFFER_SIZE       512
 
 #define MIN(a,b)    ((a) < (b)) ? (a) : (b)
-#define FSTREAM_MUTEX_LOCK()            xSemaphoreTake( __mutex, portMAX_DELAY )
-#define FSTREAM_MUTEX_UNLOCK()          xSemaphoreGive( __mutex )
 
 #define _D1(...)
 #define _D2(...)
@@ -62,50 +55,29 @@
 typedef struct {
     uint8_t *buffer;
     size_t valid_bytes;
-    ll_node_t node;
+    bool last;
 } fstream_buffer_t;
 
-typedef struct {
-    uint8_t *buffer;
-    size_t valid_bytes;
-} fstream_big_buffer_t;
-
-#ifdef PERFORMANCE_METRIC_OUTPUT
-typedef struct {
-    uint32_t consumed;
-    uint32_t underflow;
-    uint32_t time;
-} frame_data_t;
-#endif
+typedef enum {
+    FSTS__IDLE,
+    FSTS__STREAMING
+} fs_task_state_t;
 
 /*----------------------------------------------------------------------------*/
 /*                            File Scoped Variables                           */
 /*----------------------------------------------------------------------------*/
-static bool __setup;
-static xSemaphoreHandle __mutex;
-static xSemaphoreHandle __data_added;
-static xSemaphoreHandle __buffer_added;
-static xSemaphoreHandle __done;
-static xTaskHandle __task_handle;
-
-static bool __eof;
-static bool __file_valid;
 static FIL __file;
 
-static fstream_big_buffer_t __big_buffer;
+static volatile fs_task_state_t __state;
+static volatile fs_task_state_t __goal;
 
-static ll_list_t __active;
-static ll_list_t __idle;
+static fstream_buffer_t __big_buffer;
 
-fstream_malloc_fct __malloc_fn;
+static xQueueHandle __active;
+static xQueueHandle __idle;
+static xSemaphoreHandle __wakeup;
+
 fstream_free_fct __free_fn;
-
-#ifdef PERFORMANCE_METRIC_OUTPUT
-uint32_t no_data_count;
-uint32_t no_buffers_count;
-
-frame_data_t __frame[3000];
-#endif
 
 /*----------------------------------------------------------------------------*/
 /*                             Function Prototypes                            */
@@ -121,64 +93,45 @@ bool fstream_init( const uint32_t priority,
                    fstream_free_fct free_fn )
 {
     int32_t i;
+    uint32_t queue_size;
 
     _D1( "%s( %lu, %p, %p )\n", __func__, priority, malloc_fn, free_fn );
 
-#ifdef PERFORMANCE_METRIC_OUTPUT
-    no_data_count = 0;
-    no_buffers_count = 0;
-#endif
-
     /* Free is optional. */
-    if( (true == __setup) || (NULL == malloc_fn) ) {
+    if( NULL == malloc_fn ) {
         return false;
     }
 
-    __malloc_fn = malloc_fn;
+    __state = FSTS__IDLE;
+    __goal = FSTS__IDLE;
+
     __free_fn = free_fn;
 
-    __mutex = xSemaphoreCreateMutex();
+    vSemaphoreCreateBinary( __wakeup );
+    xSemaphoreTake( __wakeup, 0 );
 
-    vSemaphoreCreateBinary( __data_added );
-    vSemaphoreCreateBinary( __buffer_added );
-    vSemaphoreCreateBinary( __done );
-    xSemaphoreTake( __data_added, 0 );
-    xSemaphoreTake( __buffer_added, 0 );
-    xSemaphoreTake( __done, 0 );
+    queue_size = FSTREAM_TOTAL_BUFFER_SIZE / FSTREAM_SMALL_BUFFER_SIZE;
 
-    ll_init_list( &__active );
-    ll_init_list( &__idle );
+    __idle = xQueueCreate( queue_size, sizeof(fstream_buffer_t*) );
+    __active = xQueueCreate( queue_size, sizeof(fstream_buffer_t*) );
 
     __big_buffer.valid_bytes = 0;
 
-    __big_buffer.buffer = (uint8_t *)(*__malloc_fn)( FSTREAM_BIG_BUFFER_SIZE + FSTREAM_SMALL_BUFFER_SIZE );
+    __big_buffer.buffer = (uint8_t *)(*malloc_fn)( FSTREAM_BIG_BUFFER_SIZE + FSTREAM_SMALL_BUFFER_SIZE );
 
     for( i = 0; i < FSTREAM_TOTAL_BUFFER_SIZE; i += FSTREAM_SMALL_BUFFER_SIZE ) {
         fstream_buffer_t *n;
 
-        n = (fstream_buffer_t*) (*__malloc_fn)( sizeof(fstream_buffer_t) );
-        n->buffer = (uint8_t *) (*__malloc_fn)( FSTREAM_SMALL_BUFFER_SIZE );
+        n = (fstream_buffer_t*) (*malloc_fn)( sizeof(fstream_buffer_t) );
+        n->buffer = (uint8_t *) (*malloc_fn)( FSTREAM_SMALL_BUFFER_SIZE );
         n->valid_bytes = 0;
-        ll_init_node( &n->node, n );
-        ll_append( &__idle, &n->node );
+
+        xQueueSendToBack( __idle, &n, 0 );
     }
 
-    __file_valid = false;
-    __eof = false;
-
-    /* Make sure we can create the tread & suspend it. */
-    if( pdPASS != xTaskCreate(__task, (signed portCHAR *) "FSTR",
-                              FSTREAM_TASK_STACK_SIZE, NULL, priority,
-                              &__task_handle) )
-    {
-        if( NULL != __free_fn ) {
-            (*__free_fn)( __big_buffer.buffer );
-        }
-        __big_buffer.valid_bytes = 0;
-        return false;
-    }
-
-    __setup = true;
+    xTaskCreate( __task, (signed portCHAR *) "FSTR",
+                 FSTREAM_TASK_STACK_SIZE, NULL, priority,
+                 NULL );
 
     return true;
 }
@@ -187,81 +140,65 @@ bool fstream_init( const uint32_t priority,
 bool fstream_open( const char *filename )
 {
     _D1( "%s( %p )\n", __func__, filename );
-    if( (false == __setup) || (false != __file_valid) || (NULL == filename) ) {
+    if( NULL == filename ) {
+        _D1( "%s( %p ) -> failure\n", __func__, filename );
         return false;
     }
 
-    /* The thread is suspended */
+    fstream_close();
 
     if( FR_OK != f_open(&__file, filename, FA_READ|FA_OPEN_EXISTING) ) {
+        _D1( "%s( %p ) -> failure\n", __func__, filename );
         return false;
     }
 
-    __file_valid = true;
-    __eof = false;
     __big_buffer.valid_bytes = 0;
-#ifdef PERFORMANCE_METRIC_OUTPUT
-    memset( __frame, 0, sizeof(__frame) );
-#endif
 
-    xSemaphoreTake( __done, 0 );
-    xSemaphoreGive( __buffer_added );
-    _D2( "%s:%d\n", __FILE__, __LINE__ );
-    vTaskResume( __task_handle );
+    __goal = FSTS__STREAMING;
+    xSemaphoreGive( __wakeup );
 
+    _D2( "%s( %p ) -> success\n", __func__, filename );
     return true;
 }
 
 /** Details in file-stream.h */
 void* fstream_get_buffer( const size_t wanted, size_t *got )
 {
+    bool eof;
+
     _D1( "%s( %ld, %p ) -> ?\n", __func__, wanted, got );
-    if( (false == __setup) || (0 == wanted) || (false == __file_valid) ||
-        (NULL == got) || (FSTREAM_BIG_BUFFER_SIZE < wanted) )
-    {
-        _D2( "%s( %ld, %p ) -> NULL\n", __func__, wanted, got );
+    if( (0 == wanted) || (NULL == got) || (FSTREAM_BIG_BUFFER_SIZE < wanted) ) {
+        _D1( "%s( %ld, %p ) -> NULL\n", __func__, wanted, got );
         return NULL;
     }
 
-    while( __big_buffer.valid_bytes < wanted ) {
-        ll_node_t *node;
-        bool eof;
+    eof = false;
+    while( (__big_buffer.valid_bytes < wanted) && (false == eof) ) {
+        portBASE_TYPE rv;
+        fstream_buffer_t *node;
 
-        FSTREAM_MUTEX_LOCK();
-        node = ll_remove_head( &__active );
-        eof = __eof;
-        FSTREAM_MUTEX_UNLOCK();
-
-        if( NULL == node ) {
-            if( false == eof ) {
-#ifdef PERFORMANCE_METRIC_OUTPUT
-                no_data_count++;
-#endif
-                xSemaphoreTake( __data_added, portMAX_DELAY );
-            } else {
-                /* That's it ... we have all the data there is. */
-                *got = MIN( wanted, __big_buffer.valid_bytes );
-                _D2( "%s( %ld, %p ) -> %ld\n", __func__, wanted, got, *got );
-                return __big_buffer.buffer;
-            }
+        if( FSTS__IDLE == __state ) {
+            rv = xQueueReceive( __active, &node, 0 );
         } else {
-            fstream_buffer_t *fnode;
+            rv = xQueueReceive( __active, &node, portMAX_DELAY );
+        }
 
-            fnode = (fstream_buffer_t *) node->data;
+        if( pdFALSE == rv ) {
+            eof = true;
+        } else {
             memcpy( &__big_buffer.buffer[__big_buffer.valid_bytes],
-                    fnode->buffer, fnode->valid_bytes );
-            __big_buffer.valid_bytes += fnode->valid_bytes;
-            fnode->valid_bytes = 0;
+                        node->buffer, node->valid_bytes );
 
-            FSTREAM_MUTEX_LOCK();
-            ll_append( &__idle, node );
-            FSTREAM_MUTEX_UNLOCK();
-            xSemaphoreGive( __buffer_added );
+            __big_buffer.valid_bytes += node->valid_bytes;
+
+            eof = node->last;
+            xQueueSendToBack( __idle, &node, 0 );
         }
     }
 
     *got = MIN( wanted, __big_buffer.valid_bytes );
     _D2( "%s( %ld, %p ) -> %ld (max: %ld)\n", __func__, wanted, got, *got, __big_buffer.valid_bytes );
+
     return __big_buffer.buffer;
 }
 
@@ -269,7 +206,7 @@ void* fstream_get_buffer( const size_t wanted, size_t *got )
 void fstream_release_buffer( const size_t consumed )
 {
     _D1( "%s( %ld )\n", __func__, consumed );
-    if( (true == __setup) && (consumed <= __big_buffer.valid_bytes) ) {
+    if( consumed <= __big_buffer.valid_bytes ) {
 
         if( consumed < __big_buffer.valid_bytes ) {
             memmove( __big_buffer.buffer, &__big_buffer.buffer[consumed],
@@ -279,17 +216,6 @@ void fstream_release_buffer( const size_t consumed )
         __big_buffer.valid_bytes -= consumed;
     }
 
-#ifdef PERFORMANCE_METRIC_OUTPUT
-    {
-        static uint32_t frame = 0;
-
-        __frame[frame].underflow = abdac_get_underflow();
-        __frame[frame].consumed = consumed;
-        __frame[frame].time = xTaskGetTickCount();
-        frame++;
-    }
-#endif
-
     _D2( "%s( %ld )\n", __func__, consumed );
 }
 
@@ -297,21 +223,20 @@ void fstream_release_buffer( const size_t consumed )
 void fstream_skip( const size_t skip )
 {
     _D1( "%s( %ld )\n", __func__, skip );
-    if( (true == __setup) && (true == __file_valid) ) {
-        if( skip < __big_buffer.valid_bytes ) {
-            fstream_release_buffer( skip );
-        } else {
-            size_t left = skip;
-            left -= __big_buffer.valid_bytes;
-            __big_buffer.valid_bytes = 0;
-            while( 0 < left ) {
-                size_t got, frame;
 
-                frame = MIN( left, FSTREAM_BIG_BUFFER_SIZE );
-                fstream_get_buffer( frame, &got );
-                fstream_release_buffer( frame );
-                left -= frame;
-            }
+    if( skip < __big_buffer.valid_bytes ) {
+        fstream_release_buffer( skip );
+    } else {
+        size_t left = skip;
+        left -= __big_buffer.valid_bytes;
+        __big_buffer.valid_bytes = 0;
+        while( 0 < left ) {
+            size_t got, frame;
+
+            frame = MIN( left, FSTREAM_BIG_BUFFER_SIZE );
+            fstream_get_buffer( frame, &got );
+            fstream_release_buffer( frame );
+            left -= frame;
         }
     }
     _D2( "%s( %ld )\n", __func__, skip );
@@ -320,38 +245,35 @@ void fstream_skip( const size_t skip )
 /** Details in file-stream.h */
 void fstream_close( void )
 {
-    _D1( "%s()\n", __func__ );
-    if( (true == __setup) && (true == __file_valid) ) {
-        __file_valid = false;
-        vTaskResume( __task_handle );
+    fstream_buffer_t *node;
 
-        xSemaphoreTake( __done, portMAX_DELAY );
-        f_close( &__file );
+    _D1( "%s()\n", __func__ );
+    __goal = FSTS__IDLE;
+
+    /* Clear out any active data so we unblock the thread. */
+    while( pdTRUE == xQueueReceive(__active, &node, 0) ) {
+        xQueueSendToBack( __idle, &node, 0 );
     }
+
+    /* Wait for it to shut down. */
+    while( FSTS__STREAMING == __state ) {
+        vTaskDelay( TASK_DELAY_MS(10) );
+    }
+
+    /* Clear out any active data. */
+    while( pdTRUE == xQueueReceive(__active, &node, 0) ) {
+        xQueueSendToBack( __idle, &node, 0 );
+    }
+
+    f_close( &__file );
+    __file.fsize = 0;
+    __big_buffer.valid_bytes = 0;
     _D2( "%s()\n", __func__ );
 }
 
 /** Details in file-stream.h */
 void fstream_destroy( void )
 {
-    ll_node_t *node;
-    fstream_buffer_t *n;
-
-    _D1( "%s()\n", __func__ );
-
-    FSTREAM_MUTEX_LOCK();
-    (*__free_fn)( __big_buffer.buffer );
-    __big_buffer.buffer = NULL;
-
-    node = ll_remove_head( &__idle );
-    while( NULL != node ) {
-        n = (fstream_buffer_t*) node->data;
-
-        (*__free_fn)( n->buffer );
-        (*__free_fn)( n );
-    }
-    FSTREAM_MUTEX_UNLOCK();
-
 }
 
 /** Details in file-stream.h */
@@ -366,80 +288,46 @@ uint32_t fstream_get_filesize( void )
 
 void __task( void *params )
 {
-    vTaskSuspend( NULL );
-
     while( 1 ) {
-        uint32_t read, last_requested;
-        bool first;
+        _D2( "%s:__task: FSTS__IDLE\n", __FILE__ );
+        __state = FSTS__IDLE;
+        xSemaphoreTake( __wakeup, portMAX_DELAY );
 
-        first = true;
-        read = 0;
-        last_requested = 0;
+        if( FSTS__STREAMING == __goal ) {
+            uint32_t bytes_left;
 
-        while( (true == __file_valid) && (last_requested == read) ) {
-            ll_node_t *node;
+            __state = FSTS__STREAMING;
+            bytes_left = __file.fsize;
 
-            FSTREAM_MUTEX_LOCK();
-            node = ll_remove_head( &__idle );
-            FSTREAM_MUTEX_UNLOCK();
+            /* read the file */
+            while( 0 < bytes_left ) {
+                uint32_t requested, read;
+                fstream_buffer_t *node;
 
-            if( NULL == node ) {
-                /* Sleep until someone makes some space by using the
-                 * buffers */
-#ifdef PERFORMANCE_METRIC_OUTPUT
-                no_buffers_count++;
-#endif
-                if( true == first ) {
-                    xSemaphoreGive( __data_added );
-                    first = false;
+                xQueueReceive( __idle, &node, portMAX_DELAY );
+
+                requested = MIN( FSTREAM_SMALL_BUFFER_SIZE, bytes_left );
+                if( FR_OK != f_read(&__file, node->buffer, requested, (UINT*)&read) ) {
+                    bytes_left = 0;
+                } else {
+                    if( read == requested ) {
+                        node->valid_bytes = read;
+                        bytes_left -= read;
+                        node->last = (0 == bytes_left) ? true : false;
+                        xQueueSendToBack( __active, &node, 0 );
+                    } else {
+                        bytes_left = 0;
+                    }
                 }
-                xSemaphoreTake( __buffer_added, portMAX_DELAY );
 
-                read = 0;
-                last_requested = 0;
-            } else {
-                fstream_buffer_t *fnode;
-                fnode = (fstream_buffer_t *) node->data;
-                last_requested = FSTREAM_SMALL_BUFFER_SIZE;
-                if( FR_OK != f_read(&__file, fnode->buffer, last_requested, (UINT*)&read) ) {
-                    read = 0;
+                if( FSTS__IDLE == __goal ) {
+                    bytes_left = 0;
                 }
-                fnode->valid_bytes = read;
 
-                FSTREAM_MUTEX_LOCK();
-                ll_append( &__active, node );
-                __eof = (last_requested != read);
-                FSTREAM_MUTEX_UNLOCK();
-                if( false == first ) {
-                    xSemaphoreGive( __data_added );
-                }
+                _D2( "%s:__task: bytes_left: %lu\n", __FILE__, bytes_left );
             }
+
+            _D2( "%s:__task: done\n", __FILE__ );
         }
-
-        while( true == __file_valid ) {
-            /* File is completely read in. */
-            vTaskSuspend( NULL );
-        }
-
-        /* Done with the file completely */
-        FSTREAM_MUTEX_LOCK();
-        while( NULL != __active.head ) {
-            ll_node_t *n;
-            n = ll_remove_head( &__active );
-            ((fstream_buffer_t*) n->data)->valid_bytes = 0;
-            ll_append( &__idle, n );
-        }
-        FSTREAM_MUTEX_UNLOCK();
-
-#ifdef PERFORMANCE_METRIC_OUTPUT
-        printf( "no_buffers_count: %lu no_data_count: %lu\n", no_buffers_count, no_data_count );
-        printf( "total underflow count: %lu\n", abdac_get_underflow() );
-
-        no_buffers_count = 0;
-        no_data_count = 0;
-#endif
-
-        xSemaphoreGive( __done );
-        vTaskSuspend( NULL );
     }
 }
