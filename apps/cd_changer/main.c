43d49dde3f7825ef8771f05f400a0b0a44c9e7da
Weston Schmidt <weston_schmidt@alumni.purdue.edu>
2010-02-20 08:32:19 +0000

    Update the firmware version.

--- apps/cd_changer/main.c
+++ apps/cd_changer/main.c
@@ -30,7 +30,7 @@
 
 #define ALLOW_USING_SLOW_MEMORY 1
 
-const char firmware_label[] __attribute__ ((section (".firmware_label"))) = "Testing";
+const char firmware_label[] __attribute__ ((section (".firmware_label"))) = "Crooner-1.0.2";
 
 void* pvPortMalloc( size_t size )
 {
