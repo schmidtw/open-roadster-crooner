155cc894295d28dc4713f18f24e79bb332992e45
Trent Schmidt <trent.schmidt@gmail.com>
2011-05-09 03:21:24 +0000

    Update debug statements.  Fix the memset.  The value and size were reversed.

--- library/ibus-phone-protocol/src/ibus-phone-protocol.c
+++ library/ibus-phone-protocol/src/ibus-phone-protocol.c
@@ -70,27 +70,28 @@ size_t ibus_phone_display( char *string )
     uint8_t msg_length;
 
     if( NULL == string ) {
+        _D1( "%s:%d - NULL passed in\n", __FILE__, __LINE__ );
         return 0;
     }
 
-    _D1( "%s - string `%s`\n", __FILE__, string );
+    _D1( "%s:%d - string `%s`\n", __FILE__, __LINE__, string );
 
     length = strlen( string );
     chars_displayed = (length > MAX_PHONE_DISPLAY_SIZE) ? MAX_PHONE_DISPLAY_SIZE : length;
 
-    _D1( "%s - length(%d) chars to display(%d) MAX_SIXE(%d)\n", __FILE__,
+    _D1( "%s:%d - length(%d) chars to display(%d) MAX_SIXE(%d)\n", __FILE__, __LINE__,
          length, chars_displayed, MAX_PHONE_DISPLAY_SIZE );
 
     msg_length = chars_displayed + 3;
-    _D1( "%s - msg length(%d)\n", __FILE__, msg_length );
+    _D1( "%s:%d - msg length(%d)\n", __FILE__, __LINE__, msg_length );
 
-    memset( out, sizeof(out), 0x20 );
+    memset( out, 0x20, sizeof(out) );
     out[0] = 0x23;
     out[1] = 0x43;
     out[2] = 0x07;
     memcpy( &out[3], string, chars_displayed );
 
-    _D1( "%s: `%.*s`\n", __FILE__, chars_displayed, &out[3] );
+    _D1( "%s:%d `%.*s`\n", __FILE__, __LINE__, chars_displayed, &out[3] );
 
     if( false == ibus_physical_send_message(IBUS_DEVICE__TEL, IBUS_DEVICE__IKE,
                                             out, msg_length) ) {
