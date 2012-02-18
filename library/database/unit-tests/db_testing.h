8004c37506ce22717f28fc690bcdee9bd6c1340e
Trent Schmidt <trent.schmidt@gmail.com>
2012-02-18 07:51:18 +0000

    Streamlined the unit test for database.  Now there is a large db to test creating a database against.

---
+++ library/database/unit-tests/db_testing.h
@@ -0,0 +1,21 @@
+/*
+ * db_testing.h
+ *
+ *  Created on: Feb 16, 2012
+ *      Author: trent
+ */
+
+#ifndef DB_TESTING_H_
+#define DB_TESTING_H_
+
+#include <stdint.h>
+
+typedef struct {
+    char * artist;
+    char * album;
+    char * title;
+    uint32_t track;
+} ut_song_t;
+
+
+#endif /* DB_TESTING_H_ */
--- library/database/unit-tests/print_test.c
+++ library/database/unit-tests/print_test.c
@@ -5,15 +5,25 @@
 #include "internal_database.h"
 #include "database_print.h"
 #include "add_song.h"
+#include "queued_next_song.h"
 #include "generic.h"
 #include "indexer.h"
+#include "db_testing.h"
+#include "large_db.h"
+
+void create_database( ut_song_t *db, size_t size_db );
 
 root_database_node_t rdn;
 
-media_status_t fake_play( generic_node_t * root,
-                          media_metadata_t * metadata,
-                          media_play_fn_t play_fn,
-                          char * file_location )
+media_status_t fake_play(
+    const char *filename,
+    const double gain,
+    const double peak,
+    queue_handle_t idle,
+    const size_t queue_size,
+    media_malloc_fn_t malloc_fn,
+    media_free_fn_t free_fn,
+    media_command_fn_t command_fn )
 {
     return MI_RETURN_OK;
 }
@@ -23,7 +33,7 @@ void setup_metadata( media_metadata_t * metadata, char * artist, char * album, c
     if( NULL == metadata ) {
         printf("The Metadata pointer must not be NULL\n");
     }
-    CU_ASSERT(NULL != metadata);
+    CU_ASSERT_FATAL(NULL != metadata);
     bzero(metadata, sizeof(media_metadata_t));
 
     if( NULL != artist ) {
@@ -38,80 +48,65 @@ void setup_metadata( media_metadata_t * metadata, char * artist, char * album, c
     metadata->track_number = track_num;
 }
 
+ut_song_t simple_db[] = {
+  {"A Me", "Brushfire Fairytales", "Inaudible Melodies", 1},
+  {"Jack Johnson", "Brushfire Fairytales", "Inaudible Melodies", 2},
+  {"Jack Johnson", "Brushfire Fairytales", "Middle Man", 1},
+  {"Jack Johnson", "In between dreams", "Better Together", 1},
+  {"Jack Johnson", "In between dreams", "Never Know", 2},
+  {"Cake", "Fashion Nugget", "Frank Sinatra", 1},
+  {"Cake", "Fashion Nugget", "The Distance", 2},
+  {"Cake", "Fashion Nugget", "Friend is a Four Letter Word", 3},
+  {"Cake", "Fashion Nugget", "Open Book", 4},
+  {"Cake", "Fashion Nugget", "Daria", 5},
+  {"Cake", "Fashion Nugget", "Race Car Ya-Yas", 6},
+  {"Cake", "Fashion Nugget", "I Will Survive", 7},
+  {"Cake", "Fashion Nugget", "Stickshifts and Safetybelts", 8},
+  {"Cake", "Fashion Nugget", "Perhaps, Perhaps, Perhaps", 9},
+  {"Cake", "Fashion Nugget", "It's Coming Down", 10}
+};
+
 void create_simple_database( void )
 {
+    create_database( simple_db, sizeof(simple_db)/sizeof(ut_song_t));
+}
+
+void create_database( ut_song_t *db, size_t size_db )
+{
+    int i;
     generic_node_t * group;
     song_node_t * so_n;
     ll_node_t * node;
     media_metadata_t metadata;
     
     node = get_new_generic_node(GNT_ROOT, "root");
-    CU_ASSERT( NULL != node );
+    CU_ASSERT_FATAL( NULL != node );
     rdn.initialized = true;
     rdn.root = (generic_node_t*)node->data;
     group = rdn.root;
     
-    setup_metadata(&metadata, "A Me", "Brushfire Fairytales", "Inaudible Melodies", 1);
-
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here" );
-    CU_ASSERT( NULL != so_n );
-    
-    setup_metadata(&metadata, "Jack Johnson", "Brushfire Fairytales", "Inaudible Melodies", 2);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Jack Johnson", "Brushfire Fairytales", "Middle Man", 1);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-    
-    setup_metadata(&metadata, "Jack Johnson", "In between dreams", "Better Together", 1);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-    setup_metadata(&metadata, "Jack Johnson", "In between dreams", "Never Know", 2);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Frank Sinatra", 1);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "The Distance", 2);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Friend is a Four Letter Word", 3);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Open Book", 4);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Daria", 5);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Race Car Ya-Yas", 6);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "I Will Survive", 7);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Stickshifts and Safetybelts", 8);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "Perhaps, Perhaps, Perhaps", 9);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
-
-    setup_metadata(&metadata, "Cake", "Fashion Nugget", "It's Coming Down", 10);
-    so_n = add_song_to_root(group, &metadata, fake_play, "Here");
-    CU_ASSERT( NULL != so_n );
+    for( i = size_db - 1; i >= 0; --i ) {
+        ut_song_t ut_song = db[i];
+        setup_metadata(&metadata, ut_song.artist, ut_song.album, ut_song.title, ut_song.track);
+        so_n = add_song_to_root(group, &metadata, fake_play, "UnitTest stub location");
+        CU_ASSERT( NULL != so_n );
+    }
 
     index_root( &(rdn.root->node) );
+}
+
+/* Todo: Add 8-16 gb worth of song info */
+ut_song_t normal_db[] = {
+  {"Jack Johnson", "Brushfire Fairytales", "Inaudible Melodies", 2},
+  {"Jack Johnson", "Brushfire Fairytales", "Middle Man", 1},
+};
+
+void test_build_large_database( void )
+{
+    database_purge();
+    create_database(large_db, sizeof(large_db)/sizeof(ut_song_t));
+//    database_print();
+    database_purge();
 }
 
 void print_song_info( song_node_t * node )
@@ -209,7 +204,6 @@ void test_random_song( void )
 
 void test_queued_song( void )
 {
-    int ii;
     song_node_t * so_n = NULL;
     song_node_t * backup1_n, *backup2_n, *backup3_n, *backup4_n, *backup5_n;
     queued_song_init();
@@ -278,6 +272,7 @@ void add_suites( CU_pSuite *suite )
     CU_add_test( *suite, "Test Prev Song Print", test_previous_song );
     CU_add_test( *suite, "Test Rand Song Print", test_random_song );
     CU_add_test( *suite, "Test Queued Song", test_queued_song );
+    CU_add_test( *suite, "Test indexing a large Sized DB", test_build_large_database);
 
     database_purge();
 }
